782a2ee6dfdc94a4455d586d8da82d9afd9d693c
santichapa <santi.chaparro280@gmail.com>
2024-11-22 02:17:20 -0300

    finished formdata activity

---
+++ week03/formdata.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formElem = document.getElementById('formElem');
+    formElem.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const formData = new FormData(formElem);
+      formData.append('submitted', new Date());
+      for (let key of formData.keys()) {
+        console.log(key, formData.get(key));
+      }
+    });
+});
