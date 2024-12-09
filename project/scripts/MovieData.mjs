8fd687400bdeeddeadf1a8962263972baad8c837
santichapa <santi.chaparro280@gmail.com>
2024-12-09 14:11:38 -0300

    protected api key

--- project/scripts/MovieData.mjs
+++ project/scripts/MovieData.mjs
@@ -1,10 +1,11 @@
 import { convertToJson } from "./utilities.mjs";
 
+const key = import.meta.env.key
 const url = 'https://streaming-availability.p.rapidapi.com/shows/search/filters?country=us&series_granularity=show&genres=action&order_direction=asc&order_by=rating&output_language=en&show_type=movie';
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ec1a90c421mshbeac6715d12b258p16f183jsnbb7ecf315ebf',
+		'x-rapidapi-key': `${key}`,
 		'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
 	}
 };
